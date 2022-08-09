739294cb1464f21dba1dbf6a87e4e912f9919efa
webdevych <lee.smith870327@gmail.com>
2022-08-09 21:11:49 +0000

    init

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { MainNavigator } from './src/navigation/MainNavigator'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { observer } from 'mobx-react-lite';
+import { NavigationContainer } from '@react-navigation/native';
+import { LoginNavigator } from './src/navigation/LoginNavigator'
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <NavigatorSwitch />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  )
+}
+
+const NavigatorSwitch = observer(() => {
+  if (true) {   //auth funcc
+    return <LoginNavigator />
+  }else {
+    return <MainNavigator />
+  }
+})
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Text } from 'react-native-paper'
+import { theme } from '../core/theme'
+import { getStatusBarHeight } from 'react-native-status-bar-height'
+
+export default function Header(props) {
+  return <Text style={styles.header} {...props} />
+}
+
+const styles = StyleSheet.create({
+  header: {
+    width:1000,
+    paddingBottom:10,
+    position: 'absolute',
+    top:0,
+    paddingTop: 30 + getStatusBarHeight(),
+    fontSize: 21,
+    color: theme.colors.black,
+    fontWeight: 'bold',
+    backgroundColor:theme.colors.primary,
+    textAlign:'center'
+  },
+  background:{
+    
+  }
+})
