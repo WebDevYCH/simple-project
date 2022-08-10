7e2a7d87d0ba75f81764c8cd34520c777f60a7d7
webdevych <lee.smith870327@gmail.com>
2022-08-10 06:12:48 +0000

    add login and default bottomnavbar

--- src/core/theme.js
+++ src/core/theme.js
@@ -4,6 +4,8 @@ export const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
+    active:'#252525',
+    inactive:'#909090',
     text: '#000000',
     primary: '#909090',
     secondary: '#414757',
--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,20 +1,69 @@
 import {
     createNativeStackNavigator
   } from '@react-navigation/native-stack'
-  import MainScreen from '../screen/MainScreen'
+  import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import OtherScreen from '../screen/OtherScreen'
+import ExplorerScreen from '../screen/ExplorerScreen'
+import MatchesScreen from '../screen/MatchesScreen'
+import InboxScreen from '../screen/InboxScreen'
+import ProfileScreen from '../screen/ProfileScreen'
+import Header from '../components/Header';
 
+import { theme } from '../core/theme';
 
 const Stack = createNativeStackNavigator()
   
-export function LoginNavigator() {
+export function MainNavigator() {
 return (
     <Stack.Navigator>
         <Stack.Screen
             name="MainScreen"
-            component={MainScreen}
+            component={MaineScreen}
+            options={{ headerShown: false }}
+        />
+        <Stack.Screen
+            name="OtherScreen"
+            component={OtherScreen}
             options={{ headerShown: false }}
         />
     </Stack.Navigator>
-)
-  }
-  
+    )
+}
+const Tab = createBottomTabNavigator();
+function MaineScreen() {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                // tabBarShowLabel:false,
+                tabBarLabelPosition:'below-icon',
+                header: ({ navigation, route, options }) => {
+                    return <Header>LOGO</Header>;
+                  },
+                tabBarActiveTintColor: theme.colors.active,
+                tabBarInactiveTintColor:theme.colors.inactive,
+            }}
+        >
+            <Tab.Screen 
+                name="Explorer" 
+                component={ExplorerScreen}
+                options={{
+                    // tabBarIcon: ({ color, size }) => (
+                    //     <MaterialCommunityIcons name="home" color={color} size={size} />
+                    // ),
+                }}
+            />
+            <Tab.Screen 
+                name="Matches" 
+                component={MatchesScreen} 
+            />
+            <Tab.Screen 
+                name="Inbox" 
+                component={InboxScreen} 
+            />
+            <Tab.Screen 
+                name="Profile" 
+                component={ProfileScreen} 
+            />
+        </Tab.Navigator>
+    );
+}
